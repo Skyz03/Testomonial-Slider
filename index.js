30f6f94b2c792c9c6ddb0311edd34b9bb097179c
Syz03 <aakibdiscoo@gmail.com>
2021-08-21 19:03:33 +0545

    JS added

---
+++ index.js
@@ -0,0 +1,49 @@
+let sliderCard = document.querySelectorAll(".card");
+let arrowLeft = document.querySelector('#arrow-left');
+let arrowRight = document.querySelector('#arrow-right');
+current = 0;
+
+function reset() {
+    for (let i = 0; i < sliderCard.length; i++) {
+        sliderCard[i].style.display = "none";
+    }
+}
+
+function initialSlider() {
+    reset();
+    sliderCard[0].style.display = "flex";
+}
+
+function slideLeft() {
+    reset();
+    sliderCard[current - 1].style.display = "flex";
+    current--;
+}
+
+function slideRight() {
+    reset();
+    sliderCard[current + 1].style.display = "flex";
+    current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener('click', function () {
+
+    alert("got clicked")
+    // if (current === 0) {
+    //     current = sliderCard.length;
+    // }
+    // slideLeft();
+});
+
+arrowRight.addEventListener("click", function () {
+
+    console.log("got Clicke")
+
+    // if (current === sliderCard.length - 1) {
+    //     current = -1;
+    // }
+    // slideRight();
+});
+
+initialSlider();
